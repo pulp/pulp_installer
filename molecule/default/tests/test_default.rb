5decbccec2f6ac4d9c4cafdba275219c89ad07c8
Alexander Braverman <abraverm@redhat.com>
2018-07-04 10:57:04 +0300

    Remove meaningless assertion

--- molecule/default/tests/test_default.rb
+++ molecule/default/tests/test_default.rb
@@ -2,13 +2,6 @@ describe directory('/etc/') do
   its('owner') { should eq 'root' }
 end
 
-describe file('/var/lib/pulp/database/pulp.sqlite3') do
-  it { should exist }
-  its('owner') { should eq 'pulp' }
-  its('group') { should eq 'pulp' }
-  its('size') { should > 300 }
-end
-
 ['pulp_resource_manager', 'pulp_worker@1', 'pulp_worker@2'].each do |pservice|
   describe service(pservice) do
     it { should be_running }
