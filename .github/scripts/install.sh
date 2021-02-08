13db1da189b33b66631871ee690098909d9df110
Mike DePaulo <mikedep333@redhat.com>
2021-02-08 17:49:39 -0500

    Use Qemu emulation on GHA for Vagrant tests
    Adapted from:
    https://github.com/pulp/pulplift/pull/66
    "RFC: Testing nested Virtualization"
    
    Implementation Includes:
    1. Upgrade Qemu from 4.4 to 5.2 from our PPA to address a
    severe bug affecting CentOS 7 guests, they could not even validate
    SSL certs with curl / yum or create the Pulp postgres database.
    2. Upgrade the rest of the virtualization stack on Ubuntu
    3. Address the EL8 vagrant-sshfs workaround task failing due to
    a GPG signature mismatch.
    4. Workaround a bug with VM storage on the newer virtualization
    stack.
    5. Switch the boxes used on CentOS 7 for more recent updates.
    6. Reducing how long the pulp health check may take, particularly
    when there is a connection timed out.
    
    workaround #8095: FIPS failure in geerlingguy.postgresql by
    using an old version.
    https://pulp.plan.io/issues/8095
    
    workaround #7993:
    pulp_installer fails to create the database on EL7 when LANG=C.UTF-8
    https://pulp.plan.io/issues/7993
    
    fixes: #7884
    Move the pulp_installer Vagrant tests off Travis
    https://pulp.plan.io/issues/7884

--- .github/scripts/install.sh
+++ .github/scripts/install.sh
@@ -4,12 +4,30 @@ set -xmveuo pipefail
 
 sudo apt update
 
-# Updating vagrant-sshfs is necessary to support CentOS 7,8 without repos pre-configured.
-curl -L --output vagrant-sshfs_1.3.4-1_all.deb http://ftp.br.debian.org/debian/pool/main/v/vagrant-sshfs/vagrant-sshfs_1.3.4-1_all.deb
-sudo apt install ./vagrant-sshfs_1.3.4-1_all.deb
-rm ./vagrant-sshfs_1.3.4-1_all.deb
+sudo apt install software-properties-common
 
-sudo apt install software-properties-common openssh-server vagrant-libvirt libvirt-daemon-system vagrant-sshfs qemu-utils qemu-kvm cpu-checker dnsmasq
+if grep -i focal /etc/os-release ; then
+  # Virtualization stack upgrades, has Qemu 5.0
+  sudo add-apt-repository ppa:jacob/virtualisation
+
+  # Provides Qemu 5.2. It is needed because on Qemu 4.2 / 5.0 , CentOS 7 guests have a number
+  # of weird errors, including Postgres database creation and SSL certificate
+  # validation in curl and yum.
+  sudo add-apt-repository ppa:pulpproject/pulp-ci
+fi
+
+# 2.2.9 is needed for proper CentOS8 support
+VAGRANT_DEB=vagrant_2.2.9+dfsg-1ubuntu1_all.deb
+curl -L --output $VAGRANT_DEB http://mirrors.kernel.org/ubuntu/pool/universe/v/vagrant/$VAGRANT_DEB
+sudo apt install ./$VAGRANT_DEB
+rm ./$VAGRANT_DEB
+
+# Updating vagrant & vagrant-sshfs is necessary to support CentOS 7,8 without repos pre-configured.
+VAGRANT_SSHFS_DEB=vagrant-sshfs_1.3.5-1_all.deb
+curl -L --output $VAGRANT_SSHFS_DEB http://mirrors.kernel.org/ubuntu/pool/universe/v/vagrant-sshfs/$VAGRANT_SSHFS_DEB
+sudo apt install ./$VAGRANT_SSHFS_DEB
+rm ./$VAGRANT_SSHFS_DEB
+
 # This PPA doesn't exist for 20.04 (focal)
 if grep -i bionic /etc/os-release ; then
   sudo apt-add-repository --yes --update ppa:ansible/ansible
@@ -17,22 +35,61 @@ fi
 
 # Updating ansible past 2.9.6 (to 2.9.8+) is necessary to manage Fedora 32:
 # https://github.com/ansible/ansible/pull/68211
+# And to 2.9.16 for a bug with managing systemd
+# https://github.com/ansible/ansible/issues/71528#issuecomment-729778048
 if grep -i focal /etc/os-release ; then
-  # The 20.10 (groovy) version. Still Python 3.8
-  curl -L --output ansible_2.9.9+dfsg-1_all.deb http://mirrors.kernel.org/ubuntu/pool/universe/a/ansible/ansible_2.9.9+dfsg-1_all.deb
-  sudo apt install ./ansible_2.9.9+dfsg-1_all.deb
-  sudo rm ./ansible_2.9.9+dfsg-1_all.deb
+  # The 21.04 version.
+  ANSIBLE_DEB=ansible_2.9.16+dfsg-1.1_all.deb
+  curl -L --output $ANSIBLE_DEB http://mirrors.kernel.org/ubuntu/pool/universe/a/ansible/$ANSIBLE_DEB
+  sudo apt install ./$ANSIBLE_DEB
+  sudo rm ./$ANSIBLE_DEB
+else
+  sudo apt install ansible
 fi
 
-sudo apt install ansible
-sudo kvm-ok
+# qemu-kvm has gone from a tiny package to a virtual package in Qemu 5.2.
+# Workaround the PPA's virtual package not existing or properly depending on qemu-system-x86.
+sudo apt install qemu-kvm || sudo apt install qemu-system-x86
+
+sudo apt install openssh-server vagrant-libvirt libvirt-daemon-system qemu-utils cpu-checker dnsmasq
+
+# We may need to use upstream Vagrant version in the future, but for now the
+# new debs are faster.
+if /bin/false ; then
+  VAGRANT_DEB=vagrant_2.2.14_x86_64.deb
+  curl -O https://releases.hashicorp.com/vagrant/2.2.14/$VAGRANT_DEB
+  sudo apt install ./$VAGRANT_DEB
+  rm ./$VAGRANT_DEB
+
+  # Steps for vagrant-libvirt
+  sudo sed -i "s/# deb-src/deb-src/g" /etc/apt/sources.list
+  sudo apt update
+  sudo apt build-dep vagrant ruby-libvirt
+  sudo apt install qemu libvirt-daemon-system libvirt-clients ebtables dnsmasq-base
+  sudo apt install libxslt-dev libxml2-dev libvirt-dev zlib1g-dev ruby-dev
+
+  vagrant plugin install vagrant-libvirt
+  sudo vagrant plugin install vagrant-libvirt
+
+  vagrant plugin install vagrant-sshfs
+  sudo vagrant plugin install vagrant-sshfs
+fi
+
+if sudo kvm-ok ; then
+  # Speed up disk writes by switching qemu-kvm from writeback to unsafe.
+  ln -s $PWD/vagrant/settings.ci.yaml forklift/vagrant/settings.yaml
+else
+  # GHA: Fallback to Qemu emulation so long as GHA does not support nested virt
+  cp -p $PWD/.github/files/qemu/settings.yaml forklift/vagrant/settings.yaml
+fi
 sudo usermod -a -G libvirt $USER
 sudo systemctl enable --now ssh
 sudo systemctl enable --now libvirtd
-# newgrp - libvirt # This causes Travis to hang, with or without the dash
 
-# Speed up disk writes by switching qemu-kvm from writeback to unsafe.
-ln -s $PWD/vagrant/settings.ci.yaml forklift/vagrant/settings.yaml
+echo "VAGRANT PLUGINS AS USER:"
+vagrant plugin list
+echo "VAGRANT PLUGINS AS SUDO:"
+sudo vagrant plugin list
 
 free -m
 df -h
--- .github/scripts/script.sh
+++ .github/scripts/script.sh
@@ -2,16 +2,70 @@
 
 set -xmveuo pipefail
 
+group=libvirt
+# Re-launch the script with libvirt as the primary group.
+# Needed because we just created the group and added the GHA runner use to it.
+# Avoids running vagrant as root.
+if [ $(id -gn) != $group ]; then
+  exec sg $group "$0 $*"
+fi
+
 sed -i -e 's/memory: 10500/memory: 5500/g' vagrant/boxes.d/*
 sed -i -e 's/cpus: 4/cpus: 2/g' vagrant/boxes.d/*
 
 unset GEM_PATH
 unset GEM_HOME
-# We do this because we could not set newgrp earlier
-sudo vagrant up $1
+
+# Workaround limitation of EL7's old postgres 9.6:
+# https://pulp.plan.io/issues/7993
+sudo localectl set-locale en_US.UTF-8
+export LANG=en_US.UTF-8
+sudo localectl status
+localectl status
+
+# Workaround https://pulp.plan.io/issues/8095 until fixed.
+if [[ "$1" == *"fips" ]] ; then
+  cp .github/files/fips/requirements.yml ./requirements.yml
+fi
+
+# This comamnd does include vagrant-sshfs getting run, and thus using epel.
+# --no-tty --machine-readable and the grep is an overall approach to avoid clutter
+# in the output during box download, it would do a progress bar.
+set -o pipefail
+if vagrant up --no-tty --machine-readable --no-destroy-on-error --no-provision $1 | grep -v -e '^\s*$' ; then
+  /bin/true
+else
+  # Workaround inability to contact EPEL7, triggered by vagrant-sshfs
+  # trying to install fuse-sshfs.
+  # Seems only to occur on GHA, probably due to a transparent proxy or
+  # something similar in their network.
+  #
+  # The error manifests as:
+  # default epel.repo config, with a metalink:
+  # Cannot retrieve metalink for repository: epel/x86_64. Please verify its path and try again
+  # epel.repo configured to use a baseurl of osuosl:
+  # [Errno 14] HTTPS Error 302 - Found
+  # curl: (77) Problem with the SSL CA cert (path? access rights?)
+  #
+  # The curl error is seems misleading though, the files seem to be accessible,
+  # particularly when curl is run as root. They might be corrupted, but I can
+  # regenerate them.
+  # vagrant ssh $1 -- "sudo sed -i '/\[epel\]/a sslverify=0' /etc/yum.repos.d/epel.repo"
+  # vagrant sshfs --mount $1
+
+  # Workaround a bug in libvirt 6.6.0 (20.04 + virtualization ppa) on non-btrfs.
+  sudo virsh pool-start default
+  vagrant up --no-tty --machine-readable --no-destroy-on-error --no-provision $1 | grep -v -e '^\s*$'
+fi
+set +o pipefail
+
+echo 'QEMU COMMAND RUNNING. Look for "tcg" as the acceleration method:'
+ps -ef | grep qemu
+
+vagrant provision $1
 
 # If these start getting low, add a swapfile. Under / on Travis, or /mnt on GHA.
 echo "VM RAM:"
-sudo vagrant ssh $1 -c "free -m"
+vagrant ssh $1 -c "free -m"
 echo "Host RAM:"
 free -m
